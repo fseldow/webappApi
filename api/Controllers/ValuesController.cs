2cad785068dfcdf389eb4726af54c044f8a4d5cf
Xinhe Li <xinhl@microsoft.com>
2019-05-24 16:15:57 +0800

    init

---
+++ api/Controllers/ValuesController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public static string ProviderName = "TestTraceLoggingProvider";
+        public static EventSource MybasicSource = new EventSource(ProviderName);
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] dynamic values)
+        {
+            WriteLog("get post!!");
+            List<LogEntry> logs = JsonConvert.DeserializeObject<List<LogEntry>>(values.ToString());
+            foreach (var log in logs)
+            {
+                WriteLog("Msg: " + log.Msg);
+                MybasicSource.Write("TestEvent1",
+                    new { log.Level, log.Msg });
+            }
+            WriteLog("end");
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            WriteLog(value);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public static void WriteLog(string strLog)
+        {
+            StreamWriter log;
+            FileStream fileStream = null;
+            DirectoryInfo logDirInfo = null;
+            FileInfo logFileInfo;
+
+            string logFilePath = "D:\\home\\";
+            logFilePath = logFilePath + "Log-" + System.DateTime.Today.ToString("MM-dd-yyyy") + "." + "txt";
+            logFileInfo = new FileInfo(logFilePath);
+            logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
+            if (!logDirInfo.Exists) logDirInfo.Create();
+            if (!logFileInfo.Exists)
+            {
+                fileStream = logFileInfo.Create();
+            }
+            else
+            {
+                fileStream = new FileStream(logFilePath, FileMode.Append);
+            }
+            log = new StreamWriter(fileStream);
+            log.WriteLine(strLog);
+            log.Close();
+        }
+    }
+}
---
+++ api/LogEntry.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api
+{
+    [JsonObject]
+    public class LogEntry
+    {
+        [JsonProperty("Level")] public int Level { get; set; }
+        [JsonProperty("Msg")] public string Msg { get; set; }
+    }
+}
